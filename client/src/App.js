bdf124f7a3b3d5a5bcf0ec17193825cad6f9914b
Akhilesh Thakre <akhilesh.thakre8@gmail.com>
2021-08-28 21:25:27 +0530

    Modifying Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Posts from './components/Posts'
 
 const App = () => {
     return (
         <div>
             <h1>App</h1>
+            <Posts />
         </div>
     )
 }
